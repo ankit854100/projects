548434a145d077a9717fe356aab1bdc2a10af333
ankit854100 <ankit854100@gmail.com>
2022-01-30 21:37:52 +0530

    warning message added

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './project.css';
 import blockly from '../images/blockly.png'
 
@@ -14,6 +14,7 @@ function Project({ name, tech_used, url, image }) {
     function mouseOutFuncion() {
         setIsMouseOver(false);
     }
+
     return (
         <div id="project-wrapper" className='project-wrapper' onMouseEnter={mouseOverFunction} onMouseLeave={mouseOutFuncion} style={{ backgroundImage: "url(" + image + ")" }}>
             {isMouseOver ?
@@ -23,7 +24,8 @@ function Project({ name, tech_used, url, image }) {
                         <div className='tech-container'>
                             {tech_used.map((tech, index) => <span key={index}>{tech}</span>)}
                         </div>
-                        <a href={url} target="_blank">Visit Project</a>
+                        <a href={url} target="_blank" className="link">Visit Project</a>
+                        {name == 'NFL Fantasy gaming webiste' && <p className='extension'>Please use <a href="https://chrome.google.com/webstore/detail/allow-cors-access-control/lhobafahddgcelffkeicbaginigeejlf?hl=en" target="_blank">this</a> extension in order to use this website</p>}
                     </div>
                 </div>
                 : null
